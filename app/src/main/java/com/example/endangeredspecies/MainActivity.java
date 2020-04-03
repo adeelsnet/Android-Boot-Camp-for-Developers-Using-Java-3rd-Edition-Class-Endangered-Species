106b823e70894e386ae4c41789716a9384a47711
adeelsnet <adeelsnet@users.noreply.github.com>
2020-04-02 20:49:00 -0400

    first commit

---
+++ app/src/main/java/com/example/endangeredspecies/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.endangeredspecies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    int [] animals = {
+            R.drawable.eagle,
+            R.drawable.elephant,
+            R.drawable.panda,
+            R.drawable.panther,
+            R.drawable.gorilla,
+            R.drawable.polar
+    };
+    String [] animalNames = {
+            "Eagle",
+            "Elephant",
+            "Panda",
+            "Panther",
+            "Gorilla",
+            "Polar"
+    };
+    //imgLarge (bottom half of screen)
+    ImageView pic;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView grid = findViewById(R.id.gridView);
+        pic = findViewById(R.id.imgLarge);
+
+        grid.setAdapter(new ImageAdapter(this));
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Tell the user which image is clicked
+                Snackbar.make(view,
+                        "Selected Species: " + (position+1) + ":" + animalNames[position],
+                        Snackbar.LENGTH_LONG).show();
+
+                //Set the bottom ImageView's
+                pic.setImageResource(animals[position]);
+            }
+        });
+    }
+
+    private class ImageAdapter extends BaseAdapter {
+        private Context context;
+
+        public ImageAdapter(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return animals.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
+}
